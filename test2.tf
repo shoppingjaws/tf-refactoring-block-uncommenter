4c8fbbbd8566321eef3a551802d68a54bdd4de29
ShoppingJaws <25874783+shoppingjaws@users.noreply.github.com>
2025-11-19 21:40:21 +0900

    feat: 新規Terraformファイルを追加し、リソース定義とコメントアウトルールを設定

---
+++ test2.tf
@@ -0,0 +1,29 @@
+# Test file for verifying the uncommenter action
+# Testing PR creation with correct permissions
+
+resource "aws_instance" "example" {
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+}
+
+# These blocks should be automatically commented out by the action
+moved {
+  from = aws_instance.old_name
+  to   = aws_instance.example
+}
+
+import {
+  to = aws_s3_bucket.test_bucket
+  id = "my-test-bucket"
+}
+
+removed {
+  from = aws_security_group.deprecated
+  lifecycle {
+    destroy = false
+  }
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "my-test-bucket"
+}
