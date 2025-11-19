5db54a91bfaa6306071a765b694260d4db41b9a0
ShoppingJaws <25874783+shoppingjaws@users.noreply.github.com>
2025-11-19 21:20:31 +0900

    test: add comprehensive test.tf with moved/import/removed blocks
    This will trigger the action to verify pkg/ directory fix works correctly

--- test.tf
+++ test.tf
@@ -1,4 +1,28 @@
+# Test file for verifying the uncommenter action
+
+resource "aws_instance" "example" {
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+}
+
+# These blocks should be automatically commented out by the action
 moved {
-  from = "old_resource.tf"
-  to   = "new_resource.tf"
+  from = aws_instance.old_name
+  to   = aws_instance.example
+}
+
+import {
+  to = aws_s3_bucket.test_bucket
+  id = "my-test-bucket"
+}
+
+removed {
+  from = aws_security_group.deprecated
+  lifecycle {
+    destroy = false
+  }
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "my-test-bucket"
 }
